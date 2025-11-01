edcee8465c39a231c21cc82ad71f1d40c77caf40
Bobomurod Xudayberganov <khudayberganovbobomurod@gmail.com>
2025-11-01 21:09:57 +0500

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
+import { Analytics } from '@vercel/analytics/react'
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import ExplorerSidebar from './components/ExplorerSidebar'
 import Footer from './components/Footer'
 import Header from './components/Header'
 import TerminalPanel from './components/TerminalPanel'
 import About from './pages/About'
+import AdminBlog from './pages/AdminBlog'
+import Blog from './pages/Blog'
 import Contact from './pages/Contact'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import Skills from './pages/Skills'
-import Blog from './pages/Blog' 
-import AdminBlog from './pages/AdminBlog' 
 
 function App() {
 	const [isSidebarOpen, setIsSidebarOpen] = useState(false)
@@ -30,25 +31,38 @@ function App() {
 					/>
 
 					{/* Routes */}
-					<main id='scroll-container' className='flex-1 overflow-y-auto scroll-smooth'>
+					<main
+						id='scroll-container'
+						className='flex-1 overflow-y-auto scroll-smooth'
+					>
 						<Routes>
 							{/* 👇 Main SPA section */}
 							<Route
 								path='/'
 								element={
 									<div>
-										<section id='home'><Home /></section>
-										<section id='about'><About /></section>
-										<section id='skills'><Skills /></section>
-										<section id='projects'><Projects /></section>
-										<section id='contact'><Contact /></section>
+										<section id='home'>
+											<Home />
+										</section>
+										<section id='about'>
+											<About />
+										</section>
+										<section id='skills'>
+											<Skills />
+										</section>
+										<section id='projects'>
+											<Projects />
+										</section>
+										<section id='contact'>
+											<Contact />
+										</section>
 									</div>
 								}
 							/>
 
 							{/* 👇 Blog Page — alohida ochiladi */}
 							<Route path='/blog' element={<Blog />} />
-							 <Route path="/admina" element={<AdminBlog />} />
+							<Route path='/admina' element={<AdminBlog />} />
 						</Routes>
 					</main>
 
@@ -57,6 +71,7 @@ function App() {
 
 					{/* Footer */}
 					<Footer />
+					<Analytics />
 				</div>
 			</div>
 		</Router>
